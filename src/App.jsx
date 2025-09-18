3ac37d538f790b0e01587cdd3869fa897f372b0f
Abdul-Kader-Rimon <abdulkaderrimon8@gmail.com>
2025-09-18 21:47:07 +0600

    world-on-the-go

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+ 
+import './App.css'
+import Country from './components/countries/Country'
+
+function App() {
+ 
+
+  return (
+    <>
+ 
+      <h1>Vite + React</h1>
+      <Country></Country>
+ 
+    </>
+  )
+}
+
+export default App
